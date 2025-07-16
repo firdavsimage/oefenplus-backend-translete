3a0ac017f86be1fb4b284a953560f872a3f9cad7
Firdavs Omonov <113253299+firdavsimage@users.noreply.github.com>
2025-07-16 09:05:50 +0500

    Create text.py

---
+++ converter/text.py
@@ -0,0 +1,3 @@
+def convert_text(text):
+    # Oddiy kirill-lotin almashtirish (real loyihada to‘liq modul kerak)
+    return text.replace("а", "a").replace("б", "b").replace("в", "v").replace("г", "g").replace("д", "d")
